039db57eaf9f2cff7fd46ab6116ba37e09a8949d
Clifford Kovulo <cliffkkilonzo@gmail.com>
2023-10-25 14:18:06 +0300

    Resources

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import SearchForm from '@/components/SearchForm'
+import Filters from '@/components/Filters'
+import { getResources } from '@/sanity/actions'
+import ResourceCard from '@/components/ResourceCard'
+import Header from '@/components/Header'
+
+export const revalidate = 900;
+
+interface Props {
+  searchParams: { [key: string]: string | undefined }
+}
+
+const Page = async ({searchParams}: Props) => {
+ const resources = await getResources({
+    query: '',
+    category: searchParams?.category || '',
+    page: '1'
+ })
+
+ console.log(resources);
+ 
+  return (
+    <main className='flex-center paddings mx-auto  w-full max-w-screen-2xl flex-col'>
+      <section className='nav-padding w-full'>
+        <div className='flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
+          <h1 className='sm:heading1 heading2 mb-6 text-center text-white'>
+            Javascript Mastery Resources
+          </h1>
+        </div>
+        <SearchForm />
+
+      </section>
+
+
+      <Filters/>
+      <section className='flex-center mt-6 w-full flex-col sm:mt-20'>
+        
+        <Header/>
+
+        <div className='mt-12 flex w-fu;; flex-wrap justify-center gap-16 sm:justify-start'>
+            {resources?.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key = {resource._id}
+                  title = {resource.title}
+                  id = {resource._id}
+                  image = {resource.image}
+                  downloadNumber = {resource.views}
+                />
+              ))
+            ): (
+              <p className='body-regular text-white-400'>
+                No resources found
+              </p>
+            )}
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Page
---
+++ components/SearchForm.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import Image from 'next/image'
+import React, { useState, useEffect } from 'react'
+import { Input } from "@/components/ui/input"
+import { formUrlQuery } from '@/sanity/utils'
+import { useRouter, useSearchParams, usePathname } from 'next/dist/client/components/navigation'
+
+const SearchForm = () => {
+  const searchParams = useSearchParams();
+  const routes = useRouter();
+  const pathname = usePathname();
+  
+
+   const [search, setSearch] = useState('');
+
+   useEffect(() => {
+      const delayDebounceFn = setTimeout(() => {
+        let newUrl;
+        if(search) {
+            newUrl = formUrlQuery({
+            params: searchParams.toString(),
+            key: "query",
+            value:search
+          })
+        }
+
+        router.push({newUrl}, {scroll: false});
+      }, 300)
+   
+     return () => {
+       clearTimeout(delayDebounceFn);
+     }
+   }, [search])
+   
+  return (
+   <form className='flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5'>
+        <label className='flex-center relative w-full max-w-3xl'>
+            <Image
+              src="/magnifying-glass.svg"
+              className='absolute left-8'
+              width={32}
+              height={32}
+              alt='Search Icon'  
+            />
+            <Input 
+                className='base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800'
+                type='text'
+                placeholder='Search'
+                value={search}
+                onChange={(e) => setSearch(e.target.value) }
+            />
+
+        </label>
+   </form>
+  )
+}
+
+export default SearchForm
